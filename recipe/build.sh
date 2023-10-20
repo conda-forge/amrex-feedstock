d6fc2f328d3328da5258ae33d613dc79f0ee44c7
Axel Huebl <axel.huebl@plasma.ninja>
2023-10-20 00:30:56 -0700

    CMake: OpenMPI for OSX Cross-Compile
    Set the `OPAL_PREFIX` hint to find OpenMPI on OSX when
    cross-compiling for ARM64.
    
    ```
    -- Checking for module 'mpi-c'
    --   No package 'mpi-c' found
    -- Could NOT find MPI_C (missing: MPI_C_LIB_NAMES MPI_C_WORKS)
    -- Checking for module 'mpi-cxx'
    --   No package 'mpi-cxx' found
    -- Could NOT find MPI_CXX (missing: MPI_CXX_LIB_NAMES MPI_CXX_WORKS)
    CMake Error at /Users/runner/miniforge3/conda-bld/amrex_1697740909951/_build_env/share/cmake-3.27/Modules/FindPackageHandleStandardArgs.cmake:230 (message):
      Could NOT find MPI (missing: MPI_C_FOUND MPI_CXX_FOUND C CXX)
    Call Stack (most recent call first):
      /Users/runner/miniforge3/conda-bld/amrex_1697740909951/_build_env/share/cmake-3.27/Modules/FindPackageHandleStandardArgs.cmake:600 (_FPHSA_FAILURE_MESSAGE)
      /Users/runner/miniforge3/conda-bld/amrex_1697740909951/_build_env/share/cmake-3.27/Modules/FindMPI.cmake:1837 (find_package_handle_standard_args)
      Tools/CMake/AMReXParallelBackends.cmake:23 (find_package)
      Src/CMakeLists.txt:40 (include)
    ```

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,6 +28,13 @@ if [[ "$mpi" == "openmpi" ]]; then
     export OMPI_MCA_rmaps_base_oversubscribe=yes
     export OMPI_MCA_btl_vader_single_copy_mechanism=none
 fi
+# CMake cannot find OpenMPI when cross-compiling for arm64 on macOS
+if [[ "$mpi" == "openmpi" &&
+      ${target_platform} =~ osx.* &&
+      "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+
+    export OPAL_PREFIX=${PREFIX}
+fi
 
 # configure
 cmake \
